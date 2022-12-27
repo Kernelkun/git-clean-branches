e0e1e59403863ee02db78fd5cef117b6dc6a0e67
Jose Gazquez <jose.gazquez@spartacommodities.com>
2022-12-27 19:16:46 +0100

    chore: refactor main to commands.ts

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,22 @@
 #!/usr/bin/env node
-import { main } from './main';
+import { Command } from 'commander';
+import { listBranches } from './commands';
+const program = new Command();
 
-main();
+program
+  .description(
+    'CLI to check the local branches that are missing on the remote.'
+  )
+  .version('0.0.1')
+  .option('-h, --help', 'display this info', () => program.help())
+  .option(
+    '-ls, --list',
+    "list all the branches in local that doesn't exists on remote",
+    listBranches
+  )
+  .option('-g, --group', 'group by prefix [feature/fix/others]')
+  .option('-s, --silent', 'no output, just delete branches')
+  .option('-S, --sort <period>', 'sort by years, months or days [xy/xm/xd]')
+  .parse(process.argv);
+
+program.opts();
---
+++ src/commands.ts
@@ -0,0 +1,6 @@
+/**
+ * list all the branches in local that doesn't exist on remote
+ */
+export const listBranches = () => {
+  console.log('list');
+};
