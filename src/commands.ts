861286f0cd91cdfe781a992de6e40e07435673a3
Jose Gazquez <jose.gazquez@spartacommodities.com>
2022-12-27 21:19:57 +0100

    new: add list command

--- src/commands.ts
+++ src/commands.ts
@@ -1,6 +1,17 @@
+import { getLocalBranches, getRemoteBranches } from './utils';
+
 /**
  * list all the branches in local that doesn't exist on remote
  */
-export const listBranches = () => {
-  console.log('list');
+export const listBranches = async () => {
+  const local = await getLocalBranches();
+  const remote = await getRemoteBranches();
+
+  console.log(
+    local.flatMap((localBranch: string) => {
+      return remote.find((remoteBranch) => remoteBranch === localBranch)
+        ? []
+        : localBranch;
+    })
+  );
 };
---
+++ src/utils.ts
@@ -0,0 +1,60 @@
+import { exec as childExec, ExecException } from 'child_process';
+
+/**
+ * Execute a shell command.
+ * @param command The shell command to be executed.
+ * @return A Promise with the output/errors.
+ */
+const exec = (command: string) =>
+  new Promise<{ error: ExecException | null; stdout: string; stderr: string }>(
+    (resolve, reject) =>
+      childExec(command, (error, stdout, stderr) => {
+        if (error) {
+          reject(`error: ${error.message}`);
+        }
+        if (stderr) {
+          reject(`stderr: ${stderr}`);
+        }
+        resolve({ error, stdout, stderr });
+      })
+  );
+
+/**
+ * Split the stdout output and will take out all the empty lines
+ * @param stdout The stdout output of the child process.
+ * @param prefix A prefix to be ignored.
+ * @return An array with the branches.
+ */
+const split = (stdout: string, prefix = '') => {
+  return stdout
+    .split('\n')
+    .flatMap((line) =>
+      line.length > 0 ? line.trim().replace('* ', '').replace(prefix, '') : []
+    );
+};
+
+/**
+ * List all the remote branches.
+ * @return An array with the remote branches.
+ */
+export const getRemoteBranches = async () => {
+  try {
+    const { stdout } = await exec(`git branch -r`);
+    return split(stdout, 'origin/');
+  } catch {
+    throw new Error('Error getting remote branches.');
+  }
+};
+
+/**
+ * List all the local branches.
+ * @return An array with the local branches.
+ */
+export const getLocalBranches = async () => {
+  try {
+    const { stdout } = await exec(`git branch`);
+    return split(stdout);
+  } catch {
+    throw new Error('Error getting remote branches.');
+  }
+};
